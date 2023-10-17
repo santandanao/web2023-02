bd078ccb41001747c1f9352cb47db742e1d2d333
santandanao <131091831+santandanao@users.noreply.github.com>
2023-10-17 09:44:55 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -4,3 +4,14 @@ function sayhello61(){
 function saygoodbye61(){
   alert('Goodbye.');
 }
+
+var who65='world';
+function sathello65(){
+  alert('Hello,'+who65+'!');
+}
+function taro65(){
+  who65='太郎';
+  function hanako65(){
+    who65='花子';
+  }
+  
