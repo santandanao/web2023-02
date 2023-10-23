da9613dfa136678c5bc728bc7831ffdfc19f49d4
santandanao <131091831+santandanao@users.noreply.github.com>
2023-10-24 00:26:05 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,9 +1,10 @@
 var who66='world';
 function sayhello66(){
-  alert(Hello,world!);
+  alert('Hello,world!');
 }
 function taro66(){
-  who66='太郎';
-  function hanako66(){
-    who66='花子';
-  }
+  alert('太郎が来た');
+}
+function hanako66(){
+  alert('花子が来た');
+}
