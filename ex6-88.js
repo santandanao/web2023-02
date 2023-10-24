497ccea45bf8029dfca8860a198daa31b36427ae
santandanao <131091831+santandanao@users.noreply.github.com>
2023-10-24 10:47:16 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,5 +1,5 @@
 function gazo688(){
   document.getElementById('who688').innerHTML=
-    '<img src="./image/fish_maguro.png" alt="マグロ">'
+    '<img src="./image/sushi_maguro_dukushi_big.png" alt="マグロ寿司">'
   ;}
 
