076aa976c77ac1e8d06309c356a7f0b51f2a9d5d
santandanao <131091831+santandanao@users.noreply.github.com>
2023-10-31 10:16:38 +0900

    Update ex6-101.js

--- ex6-101.js
+++ ex6-101.js
@@ -2,4 +2,8 @@ function gazo6101(){
   document.getElementById('who6101').innerHTML=
     '<img src="./image/sushi_maguro_dukushi_big.png" alt="マグロ寿司">'
   ;}
+function gazo6102(){
+  document.getElementById('who6102').innerHTML=
+    '<img src="./image/sushi_maguro_dukushi_big.png" alt="マグロ寿司">'
+  ;}
 
