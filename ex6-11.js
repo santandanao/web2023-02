c092511401905ad885df856a887f7aa0f10bbdb9
santandanao <131091831+santandanao@users.noreply.github.com>
2023-11-07 10:29:59 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -13,8 +13,8 @@ if(cnt<11){
   function startfnc(){
     //関数hyoji()を1000ミリ秒間隔で呼び出す
   setInterval("hyoji()",1000);}
-  const str='Mozilla';
-  console.log(str.substr(1,2));
+//  const str='Mozilla';
+//  console.log(str.substr(1,2));
   //expected output:"oz"
-console.log(str.substr(2));
+//console.log(str.substr(2));
   //expected output:"zilla"
