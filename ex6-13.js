6358604535aedf5fc271d60b3da50c1b9a4c6d6b
santandanao <131091831+santandanao@users.noreply.github.com>
2023-11-07 10:31:56 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -2,7 +2,7 @@ function hyoji13(){
   //表示する文字
 var str13="一文字ずつ表示します。";
   //テキストボックスの文字数
-var cnt13=docoment.timer13.moji13.value.length;
+var cnt13=document.timer13.moji13.value.length;
   //文字が全部表示されているか確認
 if(cnt13<11){
   //現在より1文字多く切り出して表示
