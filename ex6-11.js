e786b8ced8a9f31a53fb0f821099072139d35c7f
santandanao <131091831+santandanao@users.noreply.github.com>
2023-11-07 09:58:56 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -4,3 +4,17 @@ function hyoji(){
   //テキストボックスの文字数
   var cnt=document.timer.moji.value.length;
   //文字が全部表示されているか確認
+if(cnt<11){
+  //現在より1文字多く切り出して表示
+  document.timer.moji.value=str.substr(0,cnt+1);}
+  else{
+    //すべて表示されたら、空文字に戻す
+  document.timer.moji.value="";}
+  function startfnc(){
+    //関数hyoji()を1000ミリ秒間隔で呼び出す
+  setInterval("hyoji()",1000);}
+  const str='Mojilla';
+  console.log(str.substr(1,2));
+  //expected output:"oz"
+console.log(str.substr(2));
+  //expected output:"zilla"
