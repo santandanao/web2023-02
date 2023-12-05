cff8e0412fb3565ec0d055261d4517a285ea6e2a
santandanao <131091831+santandanao@users.noreply.github.com>
2023-12-05 09:47:26 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function huto1(){ document.getElementById('hito').src='./image/inu2.jpg';}
+function huto1(){ document.getElementById('hito').src='./image/shinnen_aisatsu_man.png';}
 
-function hito2(){ document.getElementById('hito').src='./image/inu1.jpg';}
+function hito2(){ document.getElementById('hito').src='./image/zarei_man.png';}
