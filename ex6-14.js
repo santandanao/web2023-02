4478c31a4adac2429e25e2c71f9331e143c49c6f
santandanao <131091831+santandanao@users.noreply.github.com>
2023-12-05 10:20:29 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function otoko1(){ document.getElementById('dansei').src='./image/shinnen_aisatsu_man.png';}
+function otoko1(){ document.getElementById('dansei').src='./image/zarei_man.png';}
 
-function otoko2(){ document.getElementById('dansei').src='./image/zarei_man.png';}
+function otoko2(){ document.getElementById('dansei').src='./image/shinnen_aisatsu_man.png';}
